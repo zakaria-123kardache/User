313734322d00c9c6f48e12aa908eb3933c9556c9
unknown <zz240614@gmail.com>
2024-11-22 19:55:34 +0100

    Creat a Modal and Added functionality to add a new person via modal and add  image upload functionality with Condition if no image is uploaded

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 
 fetch('data.json')
-    .then(response => response.json()) 
+    .then(response => response.json())
     .then(data => {
-        
-        f(data); 
+
+        f(data);
     })
-    // .catch(error => console.error('Error fetching data:', error));
+// .catch(error => console.error('Error fetching data:', error));
 
 
 function f(data) {
     const container = document.getElementById('data');
-    
-  
+
+
     for (let category in data) {
         data[category].forEach(person => {
             const card = `
@@ -29,7 +29,63 @@ function f(data) {
                     </div>
                 </div>
             `;
-            container.innerHTML += card; 
+            container.innerHTML += card;
         });
     }
+}
+
+
+// 
+
+
+function addPerson() {
+    const type = document.getElementById('type').value;
+    const nom = document.getElementById('nom').value;
+    const prenom = document.getElementById('prenom').value;
+    const dateNaissance = document.getElementById('dateNaissance').value;
+    const niveau = document.getElementById('niveau').value;
+    const imageUpload = document.getElementById('imageUpload').files[0];
+
+    //image Check Condition
+    let imageSrc = "https://bootdey.com/img/Content/avatar/avatar7.png";
+    if (imageUpload) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            imageSrc = e.target.result;
+
+
+            addCard(type, nom, prenom, dateNaissance, niveau, imageSrc);
+        };
+        reader.readAsDataURL(imageUpload);
+    } else {
+
+        addCard(type, nom, prenom, dateNaissance, niveau, imageSrc);
+    }
+    // image Uplod
+
+
+    var modalElement = document.getElementById('addPersonModal');
+    var modal = bootstrap.Modal.getInstance(modalElement);
+    modal.hide();
+
+}
+
+function addCard(type, nom, prenom, dateNaissance, niveau, imageSrc) {
+    const container = document.getElementById('data');
+    const card = `
+      <div class="card user-card m-3">
+          <div class="card-header">
+              <h5>${type}</h5>
+          </div>
+          <div class="card-block">
+              <div class="user-image">
+                  <img src="${imageSrc}" class="img-radius" alt="User Profile Image">
+              </div>
+              <h6 class="f-w-600 m-t-25 m-b-10">${nom} ${prenom}</h6>
+              <p class="text-white">Born: ${dateNaissance}</p>
+              <p class="text-white">Level: ${niveau}</p>
+          </div>
+      </div>
+    `;
+    container.innerHTML += card;
 }
